abd9a4c246ec868728931b435938abd66a1f2c73
zaimo3869 <zaimo3869@gmail.com>
2022-05-28 20:47:44 +0200

    Add files via upload

---
+++ middleware/checkAdmin.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const con = require("../config/config")
+const mysql = require('mysql');
+
+module.exports = (req, res, next) => {
+  
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, `${process.env.TOKEN_KEY}`);
+    const userId = decodedToken.userId;
+
+    const string = "select isAdmin from users where id = ?";
+    // if (req.body.userId && req.body.userId !== userId) {
+    //   res.status(401).json("Utilisateur non autorisé à supprimer / modifier");
+    // } else {
+    //   next();
+    // }
+    const inserts = userId
+    const sql = mysql.format(string, inserts);
+    const createPost = con.query(sql, [userId],(error, result ) => {
+      
+        if (result = 1) {
+            res.isAdmin = result;
+            console.log(result);
+        } else {
+          next()
+        }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: error | "Requête non-authentifiée !" });
+  }
+};
